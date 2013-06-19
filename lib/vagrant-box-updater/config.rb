902f7e68511fe82a10dd58ba77682f938eb3543e
Yvonne Lam <ylam@daptiv.com>
2013-06-18 17:53:12 -0700

    Config option to auto-update vagrant boxes.

--- lib/vagrant-box-updater/config.rb
+++ lib/vagrant-box-updater/config.rb
@@ -2,13 +2,16 @@ module VagrantPlugins
   module BoxUpdater
     class Config < Vagrant.plugin('2', :config)
       attr_accessor :disable
+      attr_accessor :autoupdate
 
       def initialize
         @disable = false
+        @autoupdate = false
       end
 
       def finalize!
         @disable = false if @disable == UNSET_VALUE
+        @autoupdate = false if @autoupdate == UNSET_VALUE
       end
 
       def validate(machine)
